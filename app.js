8cc3f85c58e0675daea2f92cb3118ae6ce81d867
Reyner Veliz <reynerv@yahoo.com>
2022-07-26 15:34:53 -0600

    Install dotenv and set new port setting in app.js

--- app.js
+++ app.js
@@ -1,7 +1,8 @@
 const express = require('express');
 const app = express();
 const path = require('path');
-const port = 3000;
+require('dotenv').config();
+const port = process.env.PORT || 3000;
 
 app.set('view engine', 'ejs');
 app.set('vews', path.join(__dirname, 'views'));

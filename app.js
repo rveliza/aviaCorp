b4968a9cbd49f4c6c4b29e8910198d2dc636c1b6
Reyner Veliz <reynerv@yahoo.com>
2022-07-27 19:12:39 -0600

    Viajes nuevos

--- app.js
+++ app.js
@@ -27,6 +27,8 @@ db.once("open", () => {
 app.set('view engine', 'ejs');
 app.set('vews', path.join(__dirname, 'views'));
 
+app.use(express.urlencoded({ extended: true})); // to check req.body.
+
 app.get('/', (req, res) => {
     res.render('home');
 });
@@ -36,11 +38,26 @@ app.get('/viajes', async(req, res) => {
     res.render('viajes/index', { viajes });
 });
 
+app.get('/viajes/nuevo', (req, res) => {
+    res.render('viajes/new');
+});
+
+app.post('/viajes', async (req, res) => {
+    // res.send(req.body);
+    // {"viaje":{"aeronave":"Jet","empresa":"Seplacosa","solicitante":"RC Dalton","f_solicitud":"2022-07-14","salida":"2022-07-30","regreso":"2022-08-07","pasajeros":"fasdfdsa","notas":"fdafds"}}
+    const viaje = new Viaje(req.body.viaje);
+    // res.send(viaje);
+    // {"aeronave":"Jet","empresa":"Seplacosa","solicitante":"RC Dalton","f_solicitud":"2022-07-27T00:00:00.000Z","salida":"2022-07-30T00:00:00.000Z","regreso":"2022-07-30T00:00:00.000Z","pasajeros":"Juan Perez","notas":"Feria del Cafe","_id":"62e1df5d7d536e4db94474ed"}
+    await viaje.save();
+    res.redirect(`/viajes/${viaje._id }`)
+});
+
 app.get('/viajes/:id', async(req, res) => {
     const { id } = req.params;
     const viaje = await Viaje.findById(id);
     res.render('viajes/show', { viaje });
 });
+
 
 app.listen(port, () => {
     console.log(`http://localhost:${port}`);
